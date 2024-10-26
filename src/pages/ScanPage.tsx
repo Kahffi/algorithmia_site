82c421d39744ed755d35cf36fa6e6691ac5767bb
Kahffi <daffaalkahffi24@gmail.com>
2024-10-26 21:14:22 +0700

    qr-scanner refinment progressing

--- src/pages/ScanPage.tsx
+++ src/pages/ScanPage.tsx
@@ -1,8 +1,21 @@
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 import QrCode from "@/components/ui/QrCode";
 import { QrcodeErrorCallback, QrcodeSuccessCallback } from "html5-qrcode";
-import { useCallback } from "react";
+import { LucideCheck, LucideScanQrCode, LucideX } from "lucide-react";
+import { useCallback, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
 export default function ScanPage() {
+  const [success, setSuccess] = useState(false);
+  const navigate = useNavigate();
+
   const qrErrorCallback: QrcodeErrorCallback = useCallback((errMsg, err) => {
     console.error(errMsg);
     console.log(err.type);
@@ -12,18 +25,64 @@ export default function ScanPage() {
     (decodedText, result) => {
       alert(decodedText);
       console.log(result);
+      setSuccess(true);
     },
     []
   );
 
   return (
-    <div>
-      <QrCode
-        config={{ fps: 25, qrbox: { width: 280, height: 280 } }}
-        errorCallback={qrErrorCallback}
-        successCallback={qrSuccessCallback}
-        // cameraId={cameraId}
-      />
+    <div className="flex relative flex-col items-center h-svh px-3 pt-5">
+      <Link to={"/home"} className="ml-3 mb-5 w-full">
+        <LucideX size={30} />
+      </Link>
+      <div className="flex relative flex-col items-center gap-10 max-w-md">
+        <div className="flex flex-col w-full items-center gap-2 ">
+          <h1 className="font-semibold text-2xl inline">
+            <span>
+              <LucideScanQrCode className="inline mr-2" size={30} />
+            </span>
+            Scan QR Code
+          </h1>
+
+          <p className="text-sm">
+            Scan QR Code yang terdapat di berbagai stand yang tersedia, kamu
+            hanya dapat melakukan 1 kali scan untuk masing-masing stand
+          </p>
+        </div>
+        <QrCode
+          config={{
+            fps: 25,
+            qrbox: { width: 200, height: 200 },
+            disableFlip: true,
+          }}
+          errorCallback={qrErrorCallback}
+          successCallback={qrSuccessCallback}
+          // cameraId={cameraId}
+        />
+      </div>
+
+      {success && (
+        <div className="w-full h-full absolute top-0 flex justify-center items-center bg-pink-100/50 backdrop-blur-sm px-3">
+          <Card className="max-w-[320px] flex flex-col items-center">
+            <CardHeader>
+              <CardTitle className="text-lg text-center font-medium">
+                Poin Berhasil Ditambahkan{" "}
+                <LucideCheck className="inline" color="green" />
+              </CardTitle>
+            </CardHeader>
+            <CardContent className="text-sm text-center">
+              10 Poin telah ditambahkan ke akunmu.
+              <br />
+              Kumpulkan poin sebanyak-banyaknya dan menangkan hadiah menarik!
+            </CardContent>
+            <CardFooter className="w-full">
+              <Button className="w-full" onClick={() => navigate("/home")}>
+                Kembali ke Beranda
+              </Button>
+            </CardFooter>
+          </Card>
+        </div>
+      )}
     </div>
   );
 }
